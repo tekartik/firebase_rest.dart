63139adc8dc627455e062e87d90e0efac651450b
Alexandre Roux <alex@tekartik.com>
2024-06-19 14:11:56 +0200

    deps: remove dart:html dep

--- auth_rest/example/example_common.dart
+++ auth_rest/example/example_common.dart
@@ -1,9 +1,9 @@
-import 'dart:html';
-
 import 'package:tekartik_common_utils/out_buffer.dart';
+import 'package:web/web.dart';
 
 OutBuffer _outBuffer = OutBuffer(100);
 Element? _output = document.getElementById('output');
+
 void write([Object? message]) {
   print(message);
   _output!.text = (_outBuffer..add('$message')).toString();
