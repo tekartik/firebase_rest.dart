67f9dd8a69ef6e63bde2dc132233d34e0e788ae0
Alexandre Roux <alex@tekartik.com>
2025-07-31 16:17:08 +0200

    remove browser deps

--- auth_rest/lib/src/platform/platform_web.dart
+++ auth_rest/lib/src/platform/platform_web.dart
@@ -1,9 +1,6 @@
 import 'package:tekartik_firebase_auth_rest/auth_rest.dart';
 import 'package:tekartik_firebase_auth_rest/src/google_auth_rest.dart';
 
-export 'package:tekartik_firebase_auth_rest/src/google_auth_rest_web.dart'
-    show GoogleAuthProviderRestWeb;
-
 abstract class GoogleAuthProviderRestIo implements GoogleRestAuthProvider {
   factory GoogleAuthProviderRestIo({
     required GoogleAuthOptions options,
