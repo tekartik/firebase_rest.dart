3c32560ea229e95b6dfb1d26fb06157c5d3af5f3
Alexandre Roux <alex@tekartik.com>
2026-06-04 10:26:39 +0200

    feat: enhance Firebase functions callable with HTTP support and error handling

--- functions_call_rest/lib/src/functions_call_rest.dart
+++ functions_call_rest/lib/src/functions_call_rest.dart
@@ -1,13 +1,10 @@
-import 'package:cv/cv_json.dart';
 // ignore: depend_on_referenced_packages
 import 'package:tekartik_common_utils/common_utils_import.dart';
 import 'package:tekartik_firebase/firebase_mixin.dart';
-import 'package:tekartik_firebase_functions/firebase_functions.dart';
 import 'package:tekartik_firebase_functions_call/functions_call_mixin.dart';
+import 'package:tekartik_firebase_functions_call_http/functions_call_http_mixin.dart';
 import 'package:tekartik_firebase_rest/firebase_rest.dart';
-import 'package:tekartik_http/http.dart';
 import 'package:tekartik_http/http.dart' as http;
-import 'package:tekartik_http/http_client.dart';
 
 /// Rest call service
 final firebaseFunctionsCallServiceRest = _FirebaseFunctionsCallServiceRest();
@@ -181,109 +178,18 @@ class _FirebaseFunctionsCallableRest
   }
 
   @override
-  Future<FirebaseFunctionsCallableResultRest<T>> call<T>([
+  Future<FirebaseFunctionsCallableResult<T>> call<T>([
     Object? parameters,
   ]) async {
     var httpClient = functionsCallRest.client;
-
-    /*
-    /// Find current auth user if any
-    var authUserId = (functionsCallRest.app as FirebaseAppMixin)
-        .getProduct<FirebaseAuth>()
-        ?.currentUser
-        ?.uid;*/
     var uri = this.uri!;
-    var headers = <String, String>{
-      http.httpHeaderContentType: http.httpContentTypeJson,
-      //  firebaseFunctionsHttpHeaderUid: ?authUserId,
-    };
-    try {
-      var text = await httpClientRead(
-        httpClient,
-        httpMethodPost,
-        uri,
-        headers: headers,
-        body: {'data': parameters}.cvToJson(),
-      );
-      var response = text.jsonToMap();
-      var result = response['result'];
-      var error = response['error'];
-      if (error != null) {
-        throw httpsErrorFromJsonMap(error as Map);
-      }
-
-      return FirebaseFunctionsCallableResultRest<T>(result as T);
-    } catch (e, st) {
-      var httpsError = anyExceptionToHttpsError(e, stackTrace: st);
-      //devPrint('e: $e, httpsError: $httpsError');
-      throw httpsError;
-    }
+    return await firebaseFunctionsHttpCallUri(
+      httpClient,
+      uri,
+      parameters: parameters,
+    );
   }
 
   @override
   String toString() => 'CallableHttp($name)';
-}
-
-/// Firebase functions callable result Http.
-class FirebaseFunctionsCallableResultRest<T>
-    with FirebaseFunctionsCallableResultDefaultMixin<T>
-    implements FirebaseFunctionsCallableResult<T> {
-  /// Constructor
-  FirebaseFunctionsCallableResultRest(this.data);
-
-  @override
-  final T data;
-}
-
-/// Convert to http exception
-HttpsError anyExceptionToHttpsError(Object e, {StackTrace? stackTrace}) {
-  if (e is HttpsError) {
-    return e;
-  }
-  if (e is HttpClientException) {
-    return e.toHttpsError(stackTrace: stackTrace);
-  }
-  return HttpsError(HttpsErrorCode.internal, '$e', stackTrace);
-}
-
-/// Https error from map
-HttpsError httpsErrorFromJsonMap(Map map) {
-  // JSON
-  // {
-  //   "error": {
-  //     "status": "INVALID_ARGUMENT",
-  //     "message": "The password is too short.",
-  //     "details": { "minCharacters": 8 }
-  //   }
-  // }
-  return HttpsError(
-    map['status']?.toString() ?? HttpsErrorCode.unavailable,
-    map['message']?.toString() ?? 'no message',
-    map['details'],
-  );
-}
-
-var _map = {
-  httpStatusCodeNotFound: HttpsErrorCode.notFound,
-  httpStatusCodeUnauthorized: HttpsErrorCode.permissionDenied,
-  httpStatusCodeInternalServerError: HttpsErrorCode.internal,
-};
-
-var _statusCodeMap = Map<int, String>.from(_map)..addAll({});
-
-/// Convert status code to https error code
-String statusCodeToHttpsErrorCode(int statusCode) {
-  return _statusCodeMap[statusCode] ?? HttpsErrorCode.internal;
-}
-
-/// Convert to http exception
-extension HttpClientExceptionFirebaseFunctionsExt on HttpClientException {
-  /// Convert to https error
-  HttpsError toHttpsError({StackTrace? stackTrace}) {
-    return HttpsError(
-      statusCodeToHttpsErrorCode(statusCode),
-      '$this',
-      response.body,
-    );
-  }
 }
